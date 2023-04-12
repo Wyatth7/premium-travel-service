c95e891493264f3d987b55afc929b5e72f20921b
wyatth7 <whardin1010@gmail.com>
2023-04-12 16:36:12 -0400

    Added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgentsComponent } from './@pages/agents/agents.component';
 
-const routes: Routes = [];
+// paths: agents, trips, create
+
+const routes: Routes = [
+  { path: 'agents', component: AgentsComponent },
+  { path: '**', redirectTo: 'agents' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
