5822fdd62dbdaee772cbf1b2851b3ccfe3bbadcf
Wyatt <wyatt@highmatch.com>
2023-04-15 09:42:41 -0400

    Added trip component

--- src/app/@pages/trips/trips.component.ts
+++ src/app/@pages/trips/trips.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-trips',
   templateUrl: './trips.component.html',
-  styleUrls: ['./trips.component.scss']
+  styleUrls: ['./trips.component.scss'],
 })
-export class TripsComponent {
+export class TripsComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
